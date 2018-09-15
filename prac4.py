020c5e8bf57899dc8730c32585f18d90858651ab
Sol2018 <37330604+Sol2018@users.noreply.github.com>
2018-09-15 14:34:50 +0200

    Event handling

--- prac4.py
+++ prac4.py
@@ -36,3 +36,32 @@ def lightValue(channel):
 	value = mcp.read_adc(channel)
         volts = (value * 3.3) / float(1024)
         return str(round(100*volts/3.3,1))+" %"
+
+#setting up interrupt hanlders and ignore button for about 200ms after a click has been detected
+GPIO.add_event_detect(pins["reset"], GPIO.FALLING, callback=reset, bouncetime=200)
+GPIO.add_event_detect(pins["frequency"], GPIO.FALLING, callback=frequency, bouncetime=200)
+GPIO.add_event_detect(pins["stop"], GPIO.FALLING, callback=stop, bouncetime=200)
+GPIO.add_event_detect(pins["display"], GPIO.FALLING, callback=display, bouncetime=200)
+
+
+
+print("Time             Timer           Pot          Temp          Light")
+try:
+	while True:
+		if stop_start:
+			global val
+			val = str(datetime.now().strftime('%H:%M:%S') +"         "+ timerString() + "        " + potentiometerValue(1)+"        " + temperatureValue(0) +"        "+lightValue(2))
+			if counter < 5:
+				global counter
+				displayResults[counter] = val
+				counter = counter + 1
+			print val
+	       	timer += freq[selector]
+        	time.sleep(freq[selector])
+
+except KeyboardInterrupt:
+	GPIO.cleanup() # clean up GPIO on CTRL+C exit
+
+#release GPIO pins from their current operations to avoid breaking the RPi
+GPIO.cleanup()
+
